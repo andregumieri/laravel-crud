6bfc2b7db327957558a5b7b4d17a7f6283ada4b2
Andre Gumieri de Freitas <andregumieri@gmail.com>
2024-04-12 12:17:17 -0300

    override controller

---
+++ src/Console/Commands/Controller.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Andregumieri\LaravelCrud\Console\Commands;
+
+use Illuminate\Routing\Console\ControllerMakeCommand;
+
+class Controller extends ControllerMakeCommand
+{
+    protected function buildClass($name)
+    {
+        $replaces = [];
+        if($this->option('type') == 'service') {
+            $nameBase = preg_replace('/Controller$/', '', $name);
+            $replaces = [
+                '{{serviceNamespace}}' => $this->rootNamespace() . 'Services\\' . class_basename($this->getNamespace($nameBase)),
+                '{{serviceClass}}' => class_basename($nameBase) . 'Service',
+                '{{requestNamespace}}' => $this->rootNamespace() . 'Http\\Requests\\' . class_basename($this->getNamespace($nameBase)),
+                '{{requestClass}}' => class_basename($nameBase) . 'Request',
+            ];
+        }
+
+        $class = str_replace(
+            array_keys($replaces), array_values($replaces), parent::buildClass($name)
+        );
+
+        return $class;
+    }
+}
