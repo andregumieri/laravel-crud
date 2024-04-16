994d727e6d1665a5e9a9144b76c0ab1ba8941fea
Andre Gumieri de Freitas <andregumieri@gmail.com>
2024-04-16 19:59:13 -0300

    locale in crud command names

--- src/Console/Commands/Crud.php
+++ src/Console/Commands/Crud.php
@@ -13,7 +13,7 @@ class Crud extends Command
      *
      * @var string
      */
-    protected $signature = 'make:crud {singular} {plural?}';
+    protected $signature = 'make:crud {singular} {plural?} {--locale=en}';
 
     /**
      * The console command description.
@@ -21,6 +21,28 @@ class Crud extends Command
      * @var string
      */
     protected $description = 'Cria controller, service, repository, model, migration, policy';
+
+    const TRANSLATE = [
+        'pt_BR' => [
+            'CreateService' => 'CriarService',
+            'DeleteService' => 'DeletarService',
+            'UpdateService' => 'AlterarService',
+            'ViewService' => 'VerService',
+            'ListService' => 'ListarService',
+
+            'CreateController' => 'CriarController',
+            'DeleteController' => 'DeletarController',
+            'UpdateController' => 'AlterarController',
+            'ViewController' => 'VerController',
+            'ListController' => 'ListarController',
+
+            'CreateRequest' => 'CriarRequest',
+            'DeleteRequest' => 'DeletarRequest',
+            'UpdateRequest' => 'AlterarRequest',
+            'ViewRequest' => 'VerRequest',
+            'ListRequest' => 'ListarRequest',
+        ]
+    ];
 
     /**
      * Execute the console command.
@@ -37,30 +59,23 @@ class Crud extends Command
         Artisan::call('make:repository ' . $singularClass);
 
         // @todo Alterar o stub para já chamar o repository fazendo a ação que deve fazer
-        Artisan::call('make:service ' . $pluralClass . '/CriarService');
-        Artisan::call('make:service ' . $pluralClass . '/DeletarService');
-        Artisan::call('make:service ' . $pluralClass . '/AlterarService');
-        Artisan::call('make:service ' . $pluralClass . '/VerService');
-        Artisan::call('make:service ' . $pluralClass . '/ListarService');
+        foreach(['CreateService', 'DeleteService', 'UpdateService', 'ViewService', 'ListService'] as $key) {
+            Artisan::call(sprintf('make:service %s/%s', $pluralClass, $this->string($key)));
+        }
 
         // @todo Alterar o stub para já chamar a service
-        Artisan::call('make:controller ' . $pluralClass . '/CriarController' . ' --type=service');
-        Artisan::call('make:controller ' . $pluralClass . '/DeletarController' . ' --type=service');
-        Artisan::call('make:controller ' . $pluralClass . '/AlterarController' . ' --type=service');
-        Artisan::call('make:controller ' . $pluralClass . '/VerController' . ' --type=service');
-        Artisan::call('make:controller ' . $pluralClass . '/ListarController' . ' --type=service');
+        foreach(['CreateController', 'DeleteController', 'UpdateController', 'ViewController', 'ListController'] as $key) {
+            Artisan::call(sprintf('make:controller %s/%s --type=service --with-resource=%s/%s', $pluralClass, $this->string($key), $singularClass, $singularClass));
+        }
 
-        // @todo Alterar o stub para que o gate já esteja preenchido
-        Artisan::call('make:request ' . $pluralClass . '/CriarRequest');
-        Artisan::call('make:request ' . $pluralClass . '/DeletarRequest');
-        Artisan::call('make:request ' . $pluralClass . '/AlterarRequest');
-        Artisan::call('make:request ' . $pluralClass . '/VerRequest');
-        Artisan::call('make:request ' . $pluralClass . '/ListarRequest');
+        foreach(['CreateRequest', 'DeleteRequest', 'UpdateRequest', 'ViewRequest', 'ListRequest'] as $key) {
+            Artisan::call(sprintf('make:request %s/%s', $pluralClass, $this->string($key)));
+        }
 
         Artisan::call(sprintf('make:resource %s/%s', $singularClass, $singularClass));
         Artisan::call(sprintf('make:resource %s/%sCollection', $singularClass, $singularClass));
 
-        Artisan::call('make:policy ' . $singularClass . 'Policy --model=' . $singularClass);
+        Artisan::call(sprintf('make:policy %sPolicy --model=%s', $singularClass, $singularClass));
 
         $this->alert('Gates: ' . app_path('Providers/AuthServiceProvider.php'));
         $this->line(sprintf('Gate::define(\'%s-view-any\', [%sPolicy::class, \'viewAny\']);', $singularString, $singularClass));
@@ -73,11 +88,19 @@ class Crud extends Command
 
         $this->alert('Routes: ' . base_path('routes/api.php'));
         $this->line(sprintf('Route::prefix(\'%s\')->middleware(\'auth:api\')->group(function() {', $pluralString));
-        $this->line("\t" . sprintf('Route::get(\'/\', \App\Http\Controllers\%s\%s::class);', $pluralClass, 'ListarController'));
-        $this->line("\t" . sprintf('Route::post(\'/\', \App\Http\Controllers\%s\%s::class);', $pluralClass, 'CriarController'));
-        $this->line("\t" . sprintf('Route::put(\'/{%s}\', \App\Http\Controllers\%s\%s::class);', $singularString, $pluralClass, 'AlterarController'));
-        $this->line("\t" . sprintf('Route::get(\'/{%s}\', \App\Http\Controllers\%s\%s::class);', $singularString, $pluralClass, 'VerController'));
-        $this->line("\t" . sprintf('Route::delete(\'/{%s}\', \App\Http\Controllers\%s\%s::class);', $singularString, $pluralClass, 'DeletarController'));
+        $this->line("\t" . sprintf('Route::get(\'/\', \App\Http\Controllers\%s\%s::class);', $pluralClass, $this->string('ListController')));
+        $this->line("\t" . sprintf('Route::post(\'/\', \App\Http\Controllers\%s\%s::class);', $pluralClass, $this->string('CreateController')));
+        $this->line("\t" . sprintf('Route::put(\'/{%s}\', \App\Http\Controllers\%s\%s::class);', $singularString, $pluralClass, $this->string('UpdateController')));
+        $this->line("\t" . sprintf('Route::get(\'/{%s}\', \App\Http\Controllers\%s\%s::class);', $singularString, $pluralClass, $this->string('ViewController')));
+        $this->line("\t" . sprintf('Route::delete(\'/{%s}\', \App\Http\Controllers\%s\%s::class);', $singularString, $pluralClass, $this->string('DeleteController')));
         $this->line('});');
+    }
+
+    private function string($key) {
+        if(!isset(self::TRANSLATE[$this->option('locale')])) {
+            return $key;
+        }
+
+        return self::TRANSLATE[$this->option('locale')][$key];
     }
 }
